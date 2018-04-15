69548bcbdc5510e4149809fc8676ccd5610ca400
AaronWr <aaron.wreschino@smail.th-koeln.de>
2018-04-15 23:26:52 +0200

    kickoff aufgabe 3

--- Kickoff_Aufgabe/main.js
+++ Kickoff_Aufgabe/main.js
@@ -1,23 +1,35 @@
 // modules
 var fs = require('fs');
 
+// constat
+const safePath = "\\staedte_sortiert.json";
 const chalk = require('chalk');
 const log = console.log;
 
 //variables
 var citiesObj;
+var citiesSortet;
+var jsonCities;
 
-// Aufgabe 1
 fs.readFile(__dirname+"/staedte.json", function(err, data)
 {
 	citiesObj = JSON.parse(data.toString()).cities;
 
-	for(var i = citiesObj.length - 1; i >= 0; i--)
-	{
-		log(
+	citiesSortet = citiesObj.sort(function (a, b) {
+		return a.population - b.population;
+	});
+	
+	jsonCities = JSON.stringify(citiesSortet);
+
+	fs.writeFile(__dirname + safePath, jsonCities, function(err) {
+			
+		for(var i = citiesObj.length - 1; i >= 0; i--)
+		{
+			log(
 			chalk.blue("name: " + citiesObj[i].name)
-		+ chalk.magentaBright("\ncountry: " + citiesObj[i].country)
-		+ chalk.green("\npopulation: " + citiesObj[i].population)
-		+ chalk.grey("\n--------------------"));
-	}
+			+ chalk.magentaBright("\ncountry: " + citiesObj[i].country)
+			+ chalk.green("\npopulation: " + citiesObj[i].population)
+			+ chalk.grey("\n--------------------"));
+		}
+	});
 });
