f3e3a3875860e614335da0290d4894b78977cafe
Natalie Tork Alinaghipour <natalie.torkal@gmail.com>
2018-04-15 21:30:34 +0200

    kickoff aufgabe 2

--- Kickoff_Aufgabe/main.js
+++ Kickoff_Aufgabe/main.js
@@ -1,6 +1,9 @@
 // modules
 var fs = require('fs');
 
+const chalk = require('chalk');
+const log = console.log;
+
 //variables
 var citiesObj;
 
@@ -11,6 +14,10 @@ fs.readFile(__dirname+"/staedte.json", function(err, data)
 
 	for(var i = citiesObj.length - 1; i >= 0; i--)
 	{
-		console.log("name: " + citiesObj[i].name + "\ncountry: " + citiesObj[i].country + "\npopulation: " + citiesObj[i].population + "\n--------------------");
+		log(
+			chalk.blue("name: " + citiesObj[i].name)
+		+ chalk.magentaBright("\ncountry: " + citiesObj[i].country)
+		+ chalk.green("\npopulation: " + citiesObj[i].population)
+		+ chalk.grey("\n--------------------"));
 	}
 });
