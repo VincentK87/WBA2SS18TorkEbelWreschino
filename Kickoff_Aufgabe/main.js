6c1eed50cbb9978fd544f5237aa19b62063b9d31
AaronWr <aaron.wreschino@smail.th-koeln.de>
2018-04-22 17:43:12 +0200

    kickoff2 Aufgabe 1

--- Kickoff_Aufgabe/main.js
+++ Kickoff_Aufgabe/main.js
@@ -13,23 +13,31 @@ var jsonCities;
 
 fs.readFile(__dirname+"/staedte.json", function(err, data)
 {
-	citiesObj = JSON.parse(data.toString()).cities;
+	fs.readFile(__dirname+"/mehr_staedte.json", function(err2, data2)
+	{
+		var tmpCities = JSON.parse(data2.toString()).cities;
+		citiesObj = JSON.parse(data.toString()).cities;
 
-	citiesSortet = citiesObj.sort(function (a, b) {
-		return a.population - b.population;
-	});
-	
-	jsonCities = JSON.stringify(citiesSortet);
-
-	fs.writeFile(__dirname + safePath, jsonCities, function(err) {
-			
-		for(var i = citiesObj.length - 1; i >= 0; i--)
-		{
-			log(
-			chalk.blue("name: " + citiesObj[i].name)
-			+ chalk.magentaBright("\ncountry: " + citiesObj[i].country)
-			+ chalk.green("\npopulation: " + citiesObj[i].population)
-			+ chalk.grey("\n--------------------"));
+		for (var j = 0; j < tmpCities.length; j++){
+			citiesObj.push(tmpCities[j]);
 		}
+		
+		citiesSortet = citiesObj.sort(function (a, b) {
+			return a.population - b.population;
+		});
+		
+		jsonCities = JSON.stringify(citiesSortet);
+
+		fs.writeFile(__dirname + safePath, jsonCities, function(err) {
+				
+			for(var i = citiesObj.length - 1; i >= 0; i--)
+			{
+				log(
+				chalk.blue("name: " + citiesObj[i].name)
+				+ chalk.magentaBright("\ncountry: " + citiesObj[i].country)
+				+ chalk.green("\npopulation: " + citiesObj[i].population)
+				+ chalk.grey("\n--------------------"));
+			}
+		});
 	});
 });
