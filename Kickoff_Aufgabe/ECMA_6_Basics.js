e3a372321d9020030ad62bf244504181a6036d09
AaronWr <aaron.wreschino@smail.th-koeln.de>
2018-05-06 22:30:21 +0200

    ecma6 Aufgabe 2

--- Kickoff_Aufgabe/ECMA_6_Basics.js
+++ Kickoff_Aufgabe/ECMA_6_Basics.js
@@ -1,7 +1,33 @@
+require('isomorphic-fetch');
+
+var tId = "567491918";
+getTweets(tId);
+
 // Aufgabe 1
-for(let x = 0; x < 10; x++) {
-	console.log(x);
-	setTimeout(function() {
-		console.log('The number is ' + x);
-	},1000);
-};
+function Aufgabe1() {
+	for(let x = 0; x < 10; x++) {
+		console.log(x);
+		setTimeout(function() {
+			console.log('The number is ' + x);
+		},1000);
+	};
+}
+
+// Aufgabe 2
+function getTweets(id) { 
+	return fetch("https://api.twitter.com/user/" + id)
+		.then( function(response) {
+			return JSON.parse(response);
+		}).then(function(response) {
+			return response.data;
+		}).then(function(tweets) {
+			return tweets.filter(function(tweet)
+			{
+				return tweet.stars > 50
+			})
+		}).then(function(tweets) {
+			return tweets.filter(function(tweet) { 
+				return tweet.rts > 50 
+		}); 
+	});
+}
