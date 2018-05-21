3f84fa6f62401d9d58b78c236a2357b74e03209e
Natalie Tork Alinaghipour <natalie.torkal@gmail.com>
2018-05-21 22:31:51 +0200

    added igdb game route

--- Dienstgeber/games/index.js
+++ Dienstgeber/games/index.js
@@ -1,3 +1,10 @@
+const igdb = require('igdb-api-node').default;
+
+const client = igdb('c1670ab9380e5f235375e32351daff78'),
+    log = response => {
+        console.log(response.url, JSON.stringify(response.body, null, 2));
+    };
+
 const express = require("express");
 const bodyParser = require('body-parser');
 const fs = require("fs");
@@ -5,18 +12,90 @@ const request = require('request');
 
 const router = express.Router();
 
-const db_path = "/games.json";
-const allGamesJson = "/allGames.json";
-const steamAPIurl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
+const db_path = "/igdb_games.json";
+const gameDataJson = "/igdb_games.json";
 
-var allGames;
+
+var gameData;
 
 /**************************
  * REST API
 **************************/
 
 router.get('/', function(req, res){
-	res.send(allGames.apps);
+
+    client.games({
+        fields: '*', // Return all fields
+        limit: 5, // Limit to 5 results
+        offset: 15 // Index offset for results
+    }).then(gameData => {
+        res.status(200).json(gameData.body);
+    }).catch(error => {
+        res.status(401).send("Fehler: " + error);
+    });
+    
+});
+
+router.get('/filtertest', function(req, res) {
+
+    /*
+    Search for up to two Atari platforms and return their names
+    */
+    client.platforms({
+        limit: 2,
+        search: 'PlayStation'
+    }, [
+        'name'
+    ]).then(log);
+
+    /*
+    Search for up to five Zelda games with release dates between 1 Jan and
+    31 Dec 2011, sorted by release date in descending order.
+    */
+    client.games({
+        filters: {
+            'release_dates.date-gt': '2010-12-31',
+            'release_dates.date-lt': '2012-01-01'
+        },
+        limit: 5,
+        offset: 0,
+        order: 'release_dates.date:desc',
+        search: 'final fantasy'
+    }, [
+        'name',
+        'release_dates.date',
+        'rating',
+        'hypes',
+        'cover'
+    ]).then(log);
+
+    /*
+    Search for two specific games by their IDs
+    */
+    client.games({
+        ids: [
+            18472,
+            18228
+        ]
+    }, [
+        'name',
+        'cover'
+    ]).then(log);
+
+    /*
+    Search for companies with 'rockstar' in their name. Return up to five
+    results sorted by name in descending order
+    */
+    client.companies({
+        field: 'name',
+        limit: 5,
+        offset: 0,
+        order: 'name:desc',
+        search: 'square enix'
+    }, [
+        'name',
+        'logo'
+    ]).then(log);
 });
 
 
@@ -24,24 +103,25 @@ router.get('/', function(req, res){
  * Functions
 **************************/
 function updateDatabase(){
-	request(steamAPIurl, function (error, response, body) {
+	request(function (error, response, body) {
 		if (!error){
-			allGames = JSON.parse(body).applist;
-			allGames.checkDate = new Date();
+			gameData = JSON.parse(body).applist;
+			gameData.checkDate = new Date();
 			
 			saveDatabase();
 		}
 	});
 };
 
+
 function loadDatabase(){
-	fs.readFile(__dirname + allGamesJson, function(err, data){
-		allGames = JSON.parse(data);
+	fs.readFile(__dirname + gameDataJson, function(err, data){
+		gameData = JSON.parse(data);
 	});
 };
 
 function saveDatabase(){
-	fs.writeFile(__dirname + allGamesJson, JSON.stringify(allGames)	, function(err){
+	fs.writeFile(__dirname + gameDataJson, JSON.stringify(gameData), function(err){
 		console.log("success saving file");
 	});	
 }
---
+++ Dienstgeber/games/steam_index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const request = require('request');
+
+const router = express.Router();
+
+const db_path = "/games.json";
+const allGamesJson = "/allGames.json";
+const steamAPIurl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
+
+var allGames;
+
+/**************************
+ * REST API
+**************************/
+
+
+router.get('/steamGames', function(req, res){
+	res.send(allGames.apps);
+});
+
+
+/**************************
+ * Functions
+**************************/
+
+function updateDatabase(){
+	request(steamAPIurl, function (error, response, body) {
+		if (!error){
+			allGames = JSON.parse(body).applist;
+			allGames.checkDate = new Date();
+			
+			saveDatabase();
+		}
+	});
+};
+
+function loadDatabase(){
+	fs.readFile(__dirname + allGamesJson, function(err, data){
+		allGames = JSON.parse(data);
+	});
+};
+
+function saveDatabase(){
+	fs.writeFile(__dirname + allGamesJson, JSON.stringify(allGames)	, function(err){
+		console.log("success saving file");
+	});	
+}
+
+
+/**************************
+ * export
+**************************/
+
+
+module.exports = {
+
+	router: router,
+
+	loadData : function () {
+		loadDatabase();
+	}
+
+}
